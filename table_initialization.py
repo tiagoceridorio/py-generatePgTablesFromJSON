7fd42192425e80a9f90dbb17b9500cf58142828b
Tiago Ceridorio <tiago@ceridorio.com>
2024-08-06 17:28:29 -0300

    corrigindo tipos

--- table_initialization.py
+++ table_initialization.py
@@ -30,32 +30,6 @@ def get_nested_table_definitions(base_table, json_obj):
                     nested_tables.update(get_nested_table_definitions(nested_table_name, item))
     return nested_tables
 
-def get_column_definitions(json_obj):
-    columns = {}
-    for key, value in json_obj.items():
-        if '$' in key:
-            continue
-        column_name = key.lower()
-        if isinstance(value, dict) and "$oid" in value:
-            columns[column_name] = "VARCHAR"
-        elif isinstance(value, dict) and "$date" in value:
-            columns[column_name] = "TIMESTAMP"
-        elif isinstance(value, bool):
-            columns[column_name] = "BOOLEAN"
-        elif isinstance(value, int):
-            columns[column_name] = "INT"
-        elif isinstance(value, float):
-            columns[column_name] = "FLOAT"
-        elif isinstance(value, str):
-            columns[column_name] = "TEXT"
-        elif isinstance(value, list) or isinstance(value, dict):
-            columns[column_name] = "JSONB"
-        else:
-            columns[column_name] = "JSONB"
-    columns['_parent_id'] = "VARCHAR"
-    logging.info(f"Generated columns: {columns}")
-    return columns
-
 def initialize_table(cursor, base_table, json_obj):
     drop_table(cursor, base_table)
     columns = get_column_definitions(json_obj)
